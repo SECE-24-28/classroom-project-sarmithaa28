04cca9c97970caeb466636a4b8134f19242a40aa
sarmithaa28 <sarmithaaangamuthu@gmail.com>
2025-12-04 22:13:59 +0530

    day 4

---
+++ day4/script/script.js
@@ -0,0 +1,122 @@
+/*console.log("hlooo..!!!");
+function myfunction(){
+    console.log("your attitude what matters");
+
+}
+function myfunction(){
+    
+  //number input
+    let x=Number(document.getElementById('xinput').value);
+    let y=Number(document.getElementById('yinput').value);
+    let e=x+y;
+    console.log(e);
+
+    usinge parse input 
+    let x=parseInt(document.getElementById('xinput').value);
+    let y=parseInt(document.getElementById('yinput').value);
+    let e=x+y;
+    console.log(e);
+    
+   let x=0;
+   if(x==0){
+    /*single = numberequal to
+    double == means check wheather if the number values is equal
+    triple === means it also checks the data type
+    console.log("true");
+   }
+    else{
+        console.log("false");
+    }
+}
+
+function myfunction(){
+    let x=parseInt(document.getElementById('xinput').value);
+    let y=parseInt(document.getElementById('yinput').value);
+    let z=parseInt(document.getElementById('zinput').value);
+    if(x>40 && y>40 && z>40)
+    {
+        console.log("pass");
+
+    }
+    else{
+        console.log("fail");
+
+    }
+
+}
+function myfunction2(){
+    let x=Number(document.getElementById('3').value);
+    
+    if (x>=90 && x<=100){
+        console.log("A+");
+    }
+    else if(x<90 && x>=80){
+        console.log("A");
+    }
+    else if(x>=70 && x<80){
+        console.log("B")
+    }
+    else{
+        console.log("Fail");
+    }
+    
+}
+function myfunction(){
+    //for loop
+    for(i=10;i<=20;i++)
+    {
+        console.log("the value is" +i)
+    }
+
+    //while loop
+    let i=0;
+    while(i<10)
+    {
+        console.log("the value is" +i)
+        i++;
+
+    }
+
+    //do-while loop
+     let i=0;
+    while(i<10)
+    {
+        console.log("the value is" +i)
+        i++;
+    }
+    do{
+        console.log("the value is"+i);
+        i++;
+    }
+    while(i>10)
+}
+
+function myfunction(){
+    let user=document.getElementById('user').value;
+    let pass=document.getElementById('pass').value;
+    let name={
+        "username":"sarmii",
+        "password":"sarmi123"
+    }
+    if(user==name.username && pass==name.password)
+    {
+        window.location.href="contact.html";
+
+    }
+    else{
+        alert(name.username)
+    }
+    
+    
+}*/
+
+function myfunction(){
+    let x=parseInt(document.getElementById('name1').value);
+    let y=parseInt(document.getElementById('name2').value);
+    let z=parseInt(document.getElementById('name3').value);
+    console.log(x+y+z);
+    console.log(x-y-z);
+    console.log(x*y*z);
+    console.log((x/y)/z);
+
+}
